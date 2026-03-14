8aeb5c5db701a1e4511cff4127106cc3ebd75dc1
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-03-15 00:40:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (85.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        int n1 = 1, n2 = 2;
+        for(int i=3; i<=n; i++){
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n2;
+    }
+};
