ed057d20b4282acd159295c578eb209f7e8476db
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-03-29 11:53:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -3,8 +3,8 @@ public:
     bool canBeEqual(string s1, string s2) {
         for(int i=0; i<2; i++){
             int j = i+2;
-            if(s1[i]==s2[i] && s2[j]==s1[j]) continue;
-            swap(s1[i], s1[j]);
+            if(s1[i] != s2[i])
+                swap(s1[i], s1[j]);
         }
         if(s1 == s2) return true;
         else return false;
