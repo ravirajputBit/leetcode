44a725015b9c37222949fcdb6a6a7b1eb60f641d
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-03-30 12:06:29 +0530

    Time: 4 ms (36.19%), Space: 28.6 MB (56.69%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for(int i=0; i<nums.size(); i++)
+            m[nums[i]]++;
+        
+        for(int i=0; i<nums.size(); i++)
+            if(nums[i]%2 == 0 && m[nums[i]] == 1)
+                return nums[i];
+        return -1;
+    }
+};
