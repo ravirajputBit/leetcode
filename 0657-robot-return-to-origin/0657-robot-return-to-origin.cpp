323e6a4e61a09d2d1bd1a87951e08dd0ebba1c08
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-04-05 23:22:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int n = moves.size();
-        if(n == 1) return false;
-
         int x = 0, y = 0;
-        
-        if(n%2 == 0){
-            for(int i=0; i<n; i++){
-                if(moves[i] == 'L' || moves[i] == 'R'){
-                    if(moves[i] == 'R') x++;
-                    else x--;
-                }
-                else{
-                    if(moves[i] == 'U') y++;
-                    else y--;
-                }
-            }
-            return (x == 0 && y == 0);
+        for(int i = 0; i<moves.size(); i++){
+            if(moves[i] == 'R') x++;
+            else if(moves[i] == 'L') x--;
+            else if(moves[i] == 'U') y++;
+            else if(moves[i] == 'D') y--;
         }
-        else 
-            return false;
+        return (x == 0 && y == 0);
     }
 };
