d918b4dd13233786b2c5f628d48240fb21f951f4
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-04-23 14:57:16 +0530

    Time: 0 ms (100%), Space: 8.4 MB (21.39%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> st;
+        while(n != 1){
+            if(st.count(n))
+                return false;
+            st.insert(n);
+            int sum = 0;
+            while(n > 0){
+                int dig = n%10;
+                sum += dig*dig;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return true;
+    }
+};
