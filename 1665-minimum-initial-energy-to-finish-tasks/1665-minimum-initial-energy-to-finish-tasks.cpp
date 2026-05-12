3d2886a87e947d87b2c273bf5c8af4738beea579
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-05-12 20:20:36 +0530

    Time: 31 ms (83.98%), Space: 97.3 MB (63.11%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [](vector<int>& a, vector<int>& b){
+            return a[1]-a[0] < b[1]-b[0];}
+        );
+
+        int ans = 0;
+        for(int i=0; i<tasks.size(); i++){
+            ans = max(ans+tasks[i][0], tasks[i][1]);
+        }
+        return ans;
+    }
+};
