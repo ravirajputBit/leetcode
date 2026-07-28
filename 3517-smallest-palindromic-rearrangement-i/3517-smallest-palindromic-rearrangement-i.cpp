951230370bda8f7d4f24a6497b7636fb2a1b9a95
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-07-29 00:33:01 +0530

    Time: 80 ms (40.88%), Space: 53.9 MB (99.12%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.length();
+        sort(s.begin(), s.begin()+n/2);
+        
+        for (int i=0; i<n/2; i++) {
+            s[n-1-i] = s[i];
+        }
+        return s;
+    }
+};
