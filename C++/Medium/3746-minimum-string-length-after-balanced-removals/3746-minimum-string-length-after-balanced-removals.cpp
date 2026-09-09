74c30cf43a53b3f0114a1a597bfb7b6b314b191e
Ravi Kumar <ravikumar.dev404@gmail.com>
2026-09-10 00:45:30 +0530

    Time: 0 ms (100%), Space: 14.8 MB (88.31%) - LeetHub

---
+++ C++/Medium/3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int a = 0, b = 0;
+        for(char ch : s){
+            if(ch == 'a') a++;
+            else b++;
+        }
+        return abs(a-b);
+    }
+};
